99ef7854388dd8447df03795dd654d0be7ed4479
Utsav Paudel <utsavpaudel29@gmail.com>
2023-02-02 15:20:38 +0545

    searching class and id

--- webscrap.py
+++ webscrap.py
@@ -4,7 +4,16 @@ from bs4 import BeautifulSoup
 
 res= requests.get('https://news.ycombinator.com/news')
 soup = BeautifulSoup(res.text, 'html.parser')
-print(soup.find(id = 'score_34616511'))
+print(soup.select('#score_34612759'))
+# . for class
+# '#' for id
+
+print(soup.select('.score'))
+
+
+
+
+
 
 
 
