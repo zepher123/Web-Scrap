b22027503165503342e98482cf3c3808f54766ea
Utsav Paudel <utsavpaudel29@gmail.com>
2023-02-02 14:59:31 +0545

    find id score tag

--- webscrap.py
+++ webscrap.py
@@ -4,6 +4,7 @@ from bs4 import BeautifulSoup
 
 res= requests.get('https://news.ycombinator.com/news')
 soup = BeautifulSoup(res.text, 'html.parser')
-print(soup.find('a'))
+print(soup.find(id = 'score_34616511'))
+
 
 
