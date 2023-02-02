3c768f4b1866497d7bca44f64ca0d3329a2c52f3
Utsav Paudel <utsavpaudel29@gmail.com>
2023-02-02 17:27:47 +0545

    print all titles from a website

--- webscrap.py
+++ webscrap.py
@@ -1,24 +1,27 @@
 import requests
 from bs4 import BeautifulSoup
 
-
-res= requests.get('https://news.ycombinator.com/news')
+res = requests.get('https://news.ycombinator.com/news')
 soup = BeautifulSoup(res.text, 'html.parser')
 
 # . for class
 # '#' for id
 
-links = (soup.select('.titleline')) #learn css selectors
+links = (soup.select('.titleline'))  # learn css selectors
 votes = (soup.select('.score'))
 
-print(votes[0].get('id')) #get attribute id and it prints value as score
 
 
+# print(votes[0].get('id')) #get attribute id and it prints value as score
 
 
+def create_custom_hacker_news(links, votes):
+    h = []
+    for idx, item in enumerate(links): #enumerate gives index
+        title = links[idx].getText()
 
+        h.append(title)
+    return(h)
 
-
-
-
+print(create_custom_hacker_news(links, votes))
 
